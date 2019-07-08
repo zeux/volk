7ec1c990e440870f3587dfe3169374cc38766e4b
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2019-07-07 21:44:59 -0700

    Fix whitespace placement

--- volk.h
+++ volk.h
@@ -33,7 +33,7 @@
 		typedef unsigned long DWORD;
 		typedef const wchar_t* LPCWSTR;
 		typedef void* HANDLE;
-		typedef struct HINSTANCE__ *HINSTANCE;
+		typedef struct HINSTANCE__* HINSTANCE;
 		typedef struct HWND__* HWND;
 		typedef struct HMONITOR__* HMONITOR;
 		typedef struct _SECURITY_ATTRIBUTES SECURITY_ATTRIBUTES;
