a03df7a76d8ae5362f071289c66207f52fa205f6
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2023-11-05 13:56:17 -0800

    Fix C++-style comment for header-only builds
    Also don't use ' to avoid triggering tokenization issues

--- volk.h
+++ volk.h
@@ -1951,8 +1951,7 @@ extern PFN_vkAcquireNextImage2KHR vkAcquireNextImage2KHR;
 
 #ifdef VOLK_IMPLEMENTATION
 #undef VOLK_IMPLEMENTATION
-// Prevent tools like dependency checkers that don't evaluate
-// macros from detecting a cyclic dependency.
+/* Prevent tools like dependency checkers from detecting a cyclic dependency */
 #define VOLK_SOURCE "volk.c"
 #include VOLK_SOURCE
 #endif
